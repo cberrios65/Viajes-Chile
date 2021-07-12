d13fc6104456bfd9c022b05861ad4dc03e47182f
Claudio Berrios <claudio.befig65@gmail.com>
2021-07-11 23:08:53 -0400

    Primera subida y commit del proyecto completo

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+/* Smooth que se desplaza desde el menu navegador a las distintas secciones, con vel 800 */
+$(document).ready(function(){
+    $("a").click(function(event){
+        event.preventDefault();
+
+        var gato = this.hash;
+        $("html").animate(
+        {
+            scrollTop: $(gato).offset().top - 86,
+        },
+        800
+        );
+    });
+});
